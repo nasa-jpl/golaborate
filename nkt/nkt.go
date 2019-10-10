76008affb4d67e3fa06e89e3ee804f50f773c105
Brandon Dube <brandon.dube@jpl.nasa.gov>
2019-10-10 16:37:25 -0700

    small changes to NKT responses

--- nkt/nkt.go
+++ nkt/nkt.go
@@ -5,9 +5,12 @@ import (
 	"bufio"
 	"context"
 	"encoding/binary"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"go/types"
+	"log"
+	"net/http"
 	"time"
 
 	"github.jpl.nasa.gov/HCIT/go-hcit/util"
@@ -70,19 +73,40 @@ type ModuleInformation struct {
 	ValueTypes map[string]types.BasicKind
 }
 
+// all of the following types are followed with a capital T for homogenaeity and
+// avoiding clashes with builtins
+
+type strT struct {
+	Str string `json:"str"`
+}
+
+type floatT struct {
+	F64 float64 `json:"f64"`
+}
+
+type uintT struct {
+	Int uint16 `json:"int"`
+}
+
+type byteT struct {
+	Int byte `json:"int"` // we won't distinguish between bytes and ints for users
+}
+
+type bufferT struct {
+	IntAry []byte `json:"int"`
+}
+
+type boolT struct {
+	Bool bool `json:"bool"`
+}
+
 // HumanPayload is a struct containing the basic types NKT devices may work with
 type HumanPayload struct {
-	// String holds a string
-	String string
-
-	// Uint16 holds a uint16
-	Uint16 uint16
+	// Bool holds a binary value
+	Bool bool
 
 	// Buffer holds raw bytes
 	Buffer []byte
-
-	// Bool holds a binary value
-	Bool bool
 
 	// Byte holds a single byte
 	Byte byte
@@ -90,13 +114,100 @@ type HumanPayload struct {
 	// Float holds a float
 	Float float64
 
+	// String holds a string
+	String string
+
+	// Uint16 holds a uint16
+	Uint16 uint16
+
 	// T holds the type of data actually contained in the payload
 	T types.BasicKind
 }
 
-// func (hp *HumanPayload) Thinned() interface{} {
-// 	return new(struct)
-// }
+// UnpackRegister converts the raw data from a register into a HumanPayload
+func UnpackRegister(b []byte, typ types.BasicKind) HumanPayload {
+	var hp HumanPayload
+	if len(b) == 0 {
+		return HumanPayload{}
+	}
+	switch typ {
+	case types.Uint16:
+		v := dataOrder.Uint16(b)
+		hp = HumanPayload{Uint16: v}
+	case types.Bool:
+		v := uint8(b[0]) == 1
+		hp = HumanPayload{Bool: v}
+	case types.String:
+		v := string(b)
+		hp = HumanPayload{String: v}
+	case types.Byte:
+		v := b[0]
+		hp = HumanPayload{Byte: v}
+	default: // default is 10x superres floating point value
+		v := dataOrder.Uint16(b)
+		hp = HumanPayload{Float: float64(v) / 10.0}
+	}
+
+	hp.T = typ
+	return hp
+}
+
+// EncodeAndRespond converts the humanpayload to a smaller struct with only one
+// field and writes it to w as JSON.
+func (hp *HumanPayload) EncodeAndRespond(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	switch hp.T {
+	case types.Bool:
+		obj := boolT{Bool: hp.Bool}
+
+		// the logic from err to the closing brace is copy pasted a bunch in here
+		err := json.NewEncoder(w).Encode(obj)
+		if err != nil {
+			fstr := fmt.Sprintf("error encoding %+v hp to JSON, %q", hp, err)
+			log.Println(fstr)
+			http.Error(w, fstr, http.StatusInternalServerError)
+		}
+	// skip bytes case, unhandled in Unpack
+	case types.Byte:
+		obj := byteT{Int: hp.Byte} // Byte -> int for consistency with uints
+
+		err := json.NewEncoder(w).Encode(obj)
+		if err != nil {
+			fstr := fmt.Sprintf("error encoding %+v hp to JSON, %q", hp, err)
+			log.Println(fstr)
+			http.Error(w, fstr, http.StatusInternalServerError)
+		}
+	case types.Float64:
+		obj := floatT{F64: hp.Float}
+
+		err := json.NewEncoder(w).Encode(obj)
+		if err != nil {
+			fstr := fmt.Sprintf("error encoding %+v hp to JSON, %q", hp, err)
+			log.Println(fstr)
+			http.Error(w, fstr, http.StatusInternalServerError)
+		}
+	case types.String:
+		obj := strT{Str: hp.String}
+
+		err := json.NewEncoder(w).Encode(obj)
+		if err != nil {
+			fstr := fmt.Sprintf("error encoding %+v hp to JSON, %q", hp, err)
+			log.Println(fstr)
+			http.Error(w, fstr, http.StatusInternalServerError)
+		}
+	case types.Uint16:
+		obj := uintT{Int: hp.Uint16}
+		err := json.NewEncoder(w).Encode(obj)
+		if err != nil {
+			fstr := fmt.Sprintf("error encoding %+v hp to JSON, %q", hp, err)
+			log.Println(fstr)
+			http.Error(w, fstr, http.StatusInternalServerError)
+		}
+
+	}
+}
 
 // crcHelper computes the two-byte CRC value in a concurrent safe way and one line
 func crcHelper(buf []byte) []byte {
@@ -234,32 +345,4 @@ func (m *Module) GetValue(addrName string) (MessagePrimitive, HumanPayload, erro
 		return MessagePrimitive{}, HumanPayload{}, err
 	}
 	return mpRecv, hp, nil
-}
-
-// UnpackRegister converts the raw data from a register into a HumanPayload
-func UnpackRegister(b []byte, typ types.BasicKind) HumanPayload {
-	var hp HumanPayload
-	if len(b) == 0 {
-		return HumanPayload{}
-	}
-	switch typ {
-	case types.Uint16:
-		v := dataOrder.Uint16(b)
-		hp = HumanPayload{Uint16: v}
-	case types.Bool:
-		v := uint8(b[0]) == 1
-		hp = HumanPayload{Bool: v}
-	case types.String:
-		v := string(b)
-		hp = HumanPayload{String: v}
-	case types.Byte:
-		v := b[0]
-		hp = HumanPayload{Byte: v}
-	default: // default is 10x superres floating point value
-		v := dataOrder.Uint16(b)
-		hp = HumanPayload{Float: float64(v) / 10.0}
-	}
-
-	hp.T = typ
-	return hp
 }
