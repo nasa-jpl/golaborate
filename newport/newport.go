d8a9f67db784c778ce83493ae7f542811dc8a03a
Brandon Dube <brandon.dube@jpl.nasa.gov>
2019-10-30 10:16:02 -0700

    rewrite XPS errors to be a bit friendlier

--- newport/newport.go
+++ newport/newport.go
@@ -1,2 +1,8 @@
-// Package newport provides an HTTP server for ESP300 and ESP301 motion controllers (as well as any others that speak their grammar).
+/*Package newport provides an HTTP server for ESP and XPS series motion controllers.
+
+This package could use some polish, the interfaces are asymmetric, where the
+ESP implements more or only the HTTP adaptor, but the XPS implements a Go-first
+implementation that is wrapped by the HTTP adapter.
+
+*/
 package newport
