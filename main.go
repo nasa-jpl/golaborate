49caaeda8936a3c8b821de515e341758eecea597
Brandon Dube <brandon.dube@jpl.nasa.gov>
2019-11-11 13:18:12 -0800

    shouldn't commit this but will delete later
    shows working andor listing...
    
    **make sure** to feed in the camera handle, it is not the device index
    
    output:
    2 cameras
    version 3.14.30038.0
    index 0 sn SIMCAM CMOS
    index 1 sn SIMCAM CMOS

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+	// ary := make([]uint64, 16)
+	// aryint32 := []int32{}
+	// hdr := (*reflect.SliceHeader)(unsafe.Pointer(&aryint32))
+	// hdr.Data = uintptr(unsafe.Pointer(&ary[0]))
+	// hdr.Len = len(ary) * 2
+	// hdr.Cap = cap(ary) * 2
+	// fmt.Println(aryint32)
+
+}
+
+// xps := newport.XPS{}
+// xps.GroupMoveAbsolute("Group6", []float64{123.456789})
+
+// esp := newport.ESP301{}
+// esp.Home(1)
