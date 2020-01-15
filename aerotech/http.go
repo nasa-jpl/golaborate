6f512bd6fc5859ea9ba156c3cdfcec720e1c23fa
Brandon Dube <brandon.dube@jpl.nasa.gov>
2020-01-15 14:43:26 -0800

    more generichttp goodness

--- aerotech/http.go
+++ aerotech/http.go
@@ -2,25 +2,9 @@ package aerotech
 
 import (
 	"github.jpl.nasa.gov/HCIT/go-hcit/generichttp"
-	"github.jpl.nasa.gov/HCIT/go-hcit/server"
 )
 
-// HTTPWrapper wraps an Ensemble for control with HTTP
-type HTTPWrapper struct {
-	// Ensemble is the embedded controller
-	*Ensemble
-
-	// HTTPWrapper is the embedded generic motion control wrapper
-	HTTPWrapper generichttp.HTTPMotionController
-}
-
 // NewHTTPWrapper returns a new HTTP wrapper with the route table pre-configured
-func NewHTTPWrapper(e *Ensemble) HTTPWrapper {
-	basic := generichttp.NewHTTPMotionController(e)
-	return HTTPWrapper{Ensemble: e, HTTPWrapper: basic}
-}
-
-// RT satisfies the HTTPer interface
-func (h HTTPWrapper) RT() server.RouteTable {
-	return h.HTTPWrapper.RouteTable
+func NewHTTPWrapper(e *Ensemble) generichttp.HTTPMotionController {
+	return generichttp.NewHTTPMotionController(e)
 }
--- newport/xpshttp.go
+++ newport/xpshttp.go
@@ -1,44 +1,10 @@
 package newport
 
 import (
-	"net/http"
-
 	"github.jpl.nasa.gov/HCIT/go-hcit/generichttp"
-	"github.jpl.nasa.gov/HCIT/go-hcit/server"
-
-	"goji.io/pat"
 )
 
-// XPSHTTPWrapper is an HTTP wrapper around an XPS motion controller.
-//
-// The API is a superset of the generic motion controller interface
-type XPSHTTPWrapper struct {
-	// XPS is the embedded XPS controller
-	*XPS
-
-	HTTPWrapper generichttp.HTTPMotionController
-}
-
 // NewXPSHTTPWrapper creates a new HTTP wrapper around an XPS controller
-func NewXPSHTTPWrapper(xps *XPS) XPSHTTPWrapper {
-	basic := generichttp.NewHTTPMotionController(xps)
-	w := XPSHTTPWrapper{XPS: xps, HTTPWrapper: basic}
-	basic.RouteTable[pat.Post("/axis/:axis/initialize")] = w.Initialize
-	return w
-}
-
-// Initialize initializes the specified axis
-func (h XPSHTTPWrapper) Initialize(w http.ResponseWriter, r *http.Request) {
-	axis := pat.Param(r, "axis")
-	err := h.XPS.Initialize(axis)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-}
-
-// RT satisfies server.HTTPer
-func (h XPSHTTPWrapper) RT() server.RouteTable {
-	return h.HTTPWrapper.RouteTable
+func NewXPSHTTPWrapper(xps *XPS) generichttp.HTTPMotionController {
+	return generichttp.NewHTTPMotionController(xps)
 }
