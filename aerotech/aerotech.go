05e956e5d89a829d4848417bd8cb5f0b3ee46464
Brandon Dube <brandon.dube@jpl.nasa.gov>
2020-01-15 15:24:05 -0800

    fix broken clamping logic

--- aerotech/aerotech.go
+++ aerotech/aerotech.go
@@ -219,11 +219,12 @@ func (e *Ensemble) SetVelocity(axis string, vel float64) error {
 // clampPos clamps an input if there is a limit set, otherwise does not modulate it
 func (e *Ensemble) clampPos(axis string, input float64, relative bool) (float64, error) {
 	var (
-		output float64
-		err    error = nil
+		current float64
+		output  float64
+		err     error = nil
 	)
 	if relative { // the input needs to be translated to an absolute to apply the limit.
-		current, err := e.GetPos(axis)
+		current, err = e.GetPos(axis)
 		if err != nil {
 			return input, err
 		}
@@ -239,6 +240,9 @@ func (e *Ensemble) clampPos(axis string, input float64, relative bool) (float64,
 		}
 	} else {
 		output = input
+	}
+	if relative {
+		output -= current
 	}
 	// if not relative, current is zero anyway, so can always subtract it
 	return output, err
