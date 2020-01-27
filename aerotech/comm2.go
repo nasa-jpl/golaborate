111b0458d20b8711f2f3deb9384f7e4a74353442
Brandon Dube <brandon.dube@jpl.nasa.gov>
2020-01-27 14:59:07 -0800

    Revert "add timeout error catching to aerotech"
    This reverts commit 20b3c98b087f553faf8e855488ed0b0552604aae.

--- aerotech/comm2.go
+++ aerotech/comm2.go
@@ -1,11 +1,6 @@
 package aerotech
 
 import (
-	"net"
-	"strings"
-	"time"
-
-	"github.com/cenkalti/backoff"
 	"github.jpl.nasa.gov/HCIT/go-hcit/comm"
 )
 
@@ -24,7 +19,6 @@ else, it scans for \n using the underlying remoteDevice.
 */
 
 func getAnyResponseFromEnsemble(rd *comm.RemoteDevice, singleByte bool) ([]byte, error) {
-	// the double retry mechanism made this function quite unfortunately complicated
 	if singleByte {
 		// timeout implemented inside the remote device
 		// we expect 1 byte, but allocate a buffer as large as the largest TCP
@@ -32,49 +26,9 @@ func getAnyResponseFromEnsemble(rd *comm.RemoteDevice, singleByte bool) ([]byte,
 		tcpFrameSize := 1500
 		buf := make([]byte, tcpFrameSize)
 		n := 0
-		var (
-			err  error = nil
-			err2 error
-		)
-		// the Ensemble controller will close the connection on its own
-		// or we may timeout if the motion is very slow.  Patch that up here.
-		op := func() error {
+		var err error = nil
+		for n == 0 && err == nil {
 			n, err = rd.Conn.Read(buf)
-			if err != nil {
-				txt := err.Error()
-				// we want to retry on timeout or resets
-				if strings.Contains(txt, "timeout") {
-					if con, ok := rd.Conn.(net.Conn); ok {
-						// need to avoid client timeouts; 1s timeout
-						// does a little bit of thrashing, but Go is fast.
-						con.SetReadDeadline(time.Now().Add(1 * time.Second))
-					}
-					return err
-				} else if strings.Contains(txt, "reset") {
-					// the connect was closed, we need to re-open it
-					err2 := rd.Open()
-					if err2 != nil {
-						return err2
-					}
-					return err
-				}
-				// other errors are captured in the closure to be handled without retrying
-				return nil
-			}
-			return nil
-		}
-		for n == 0 && err == nil && err2 == nil {
-			err2 = backoff.Retry(op, &backoff.ExponentialBackOff{
-				InitialInterval:     1 * time.Second,
-				RandomizationFactor: 0,
-				Multiplier:          1,
-				MaxInterval:         2 * time.Second, // this is bigger than it will ever be
-				// wait up to to five minutes, the shit has really hit the fan if it takes longer than this
-				MaxElapsedTime: 5 * time.Minute,
-				Clock:          backoff.SystemClock})
-		}
-		if err2 != nil && err == nil {
-			err = err2
 		}
 		return buf[:n], err // don't strip the end character because there is nothing to strip
 	}
