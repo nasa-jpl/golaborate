355ceef92cd963985a52b88fc922fd7c72f3e44b
Brandon Dube <brandon.dube@jpl.nasa.gov>
2020-03-25 13:02:34 -0700

    bye bye baby

--- vendor/github.com/astrogo/fitsio/consts.go
+++
@@ -1,9 +0,0 @@
-// Copyright 2015 The astrogo Authors.  All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-package fitsio
-
-const (
-	blockSize = 2880 // size in bytes of a FITS block
-)
--- vendor/github.com/brandondube/ringo/doc.go
+++
@@ -1,2 +0,0 @@
-// Package ringo provides fast ring buffers that support getting continuous slices, even when they roll over the end of the underlying slice.
-package ringo
