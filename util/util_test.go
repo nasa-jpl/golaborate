7748bcf7a7dcf0d5f18eaf66f389ef72e0d77461
Brandon Dube <brandon.dube@jpl.nasa.gov>
2020-04-21 09:50:25 -0700

    + unit tests

--- util/util_test.go
+++ util/util_test.go
@@ -2,6 +2,8 @@ package util_test
 
 import (
 	"fmt"
+	"testing"
+	"time"
 
 	"github.jpl.nasa.gov/bdube/golab/util"
 )
@@ -31,4 +33,72 @@ func ExampleSetBit_LSB() {
 	out := util.SetBit(255, 0, false)
 	fmt.Printf("%08b\n", out)
 	// Output: 11111110
+}
+
+func TestArangeByteForward(t *testing.T) {
+	var (
+		start byte = 10
+		end   byte = 20
+		step  byte = 1
+	)
+	arangeRes := util.ArangeByte(start, end, step)
+	for i := 0; i < len(arangeRes); i++ {
+		expected := start + (byte(i) * step)
+		if arangeRes[i] != expected {
+			t.Errorf("expected %d at position %d, got %d", expected, i, arangeRes[i])
+		}
+	}
+}
+
+func TestUniqueString(t *testing.T) {
+	inp := []string{"a", "b", "c", "a"}
+	expected := []string{"a", "b", "c"}
+	output := util.UniqueString(inp)
+	for i := 0; i < len(output); i++ {
+		if output[i] != expected[i] {
+			t.Errorf("expected %s got %s", expected[i], output[i])
+		}
+	}
+}
+
+func TestIntSliceToCSV(t *testing.T) {
+	inp := []int{1, 2, 3}
+	expected := "1,2,3"
+	out := util.IntSliceToCSV(inp)
+	if expected != out {
+		t.Errorf("expected %s got %s", expected, out)
+	}
+}
+
+func TestClampHigh(t *testing.T) {
+	var (
+		low   = 0.
+		high  = 10.
+		input = 20.
+	)
+	clamped := util.Clamp(input, low, high)
+	if clamped == input {
+		t.Errorf("expected out of range value %f to be clipped to %f < x < %f, got %f", input, low, high, clamped)
+	}
+}
+
+func TestClampLow(t *testing.T) {
+	var (
+		low   = 0.
+		high  = 10.
+		input = -1.
+	)
+	clamped := util.Clamp(input, low, high)
+	if clamped == input {
+		t.Errorf("expected out of range value %f to be clipped to %f < x < %f, got %f", input, low, high, clamped)
+	}
+}
+
+func TestSecsToDuration(t *testing.T) {
+	var dur time.Duration = 123456789
+	secs := dur.Seconds()
+	out := util.SecsToDuration(secs)
+	if out != dur {
+		t.Errorf("expected SecsToDuration to round trip, output %v != expected %v", out, dur)
+	}
 }
