2ed2940c1d8979b844e975bab06033a36d29aac9
Brandon Dube <brandon.dube@jpl.nasa.gov>
2020-05-01 10:12:22 -0700

    implement most of DAC interface

--- acromag/AP236/ap236.go
+++ acromag/AP236/ap236.go
@@ -1,6 +1,12 @@
 /*Package ap236 provides an interface to Acromag AP236 16-bit DAC modules
 
- */
+Some performance-limiting design changes are made from the C SDK provided
+by acromag.  Namely: there are Go functions for each channel configuration,
+and a call to any of them issues a transfer of the configuration to the board.
+This will prevent the last word in performance under obscure conditions (e.g.
+updating the config 10,000s of times per second) but is not generally harmful
+and simplifies interfacing to the device.
+*/
 package ap236
 
 /*
@@ -8,11 +14,15 @@ package ap236
 #include <stdlib.h>
 #include "../apcommon/apcommon.h"
 #include "AP236.h"
+#include "shim.h"
 */
 import "C"
 import (
 	"errors"
 	"fmt"
+	"unsafe"
+
+	_ "github.jpl.nasa.gov/bdube/golab/acromag/apcommon"
 )
 
 func init() {
@@ -152,14 +162,16 @@ func NewAP236(deviceIndex int) (*AP236, error) {
 		o    AP236
 		out  = &o
 		addr *C.struct_map236
+		cs   = C.CString(C.DEVICE_NAME) // untyped constant in C needs enforcement in Go
 	)
+	defer C.free(unsafe.Pointer(cs))
 	// confirmed by Kate Blanketship on Gophers slack that this
 	// is a valid way to generate the pointer that C wants
 	// see also: several ways to get the same address of the
 	// data: https://play.golang.org/p/fpkOIT9B3BB
-	o.cfg.pIdealCode = &IdealCode
-	// 0 is the device index, TODO: allow this to be specified
-	errC := C.APOpen(deviceIndex, &o.cfg.nHandle, C.DEVICE_NAME)
+	cptr := (*[8][7]C.double)(unsafe.Pointer(&IdealCode))
+	o.cfg.pIdealCode = cptr
+	errC := C.APOpen(C.int(deviceIndex), &o.cfg.nHandle, cs)
 	err := enrich(errC, "APOpen")
 	if err != nil {
 		return out, err
@@ -169,84 +181,136 @@ func NewAP236(deviceIndex int) (*AP236, error) {
 	if err != nil {
 		return out, err
 	}
-	C.GetAPAddress(o.cfg.nHandle, &addr)
-	o.cfg.brd_ptr = *addr
+	errC = C.GetAPAddress2(o.cfg.nHandle, &addr)
+	err = enrich(errC, "GetAPAddress")
+	if err != nil {
+		return out, err
+	}
+	o.cfg.brd_ptr = addr
 	o.cfg.bInitialized = C.TRUE
 	o.cfg.bAP = C.TRUE
 	return out, nil
 }
 
 // SetRange configures the output range of the DAC
-func (dac *AP236) SetRange(rng OutputRange) error {
+// this function only returns an error if the range is not allowed
+func (dac *AP236) SetRange(channel int, rng OutputRange) error {
 	if rng < TenVSymm || rng > TwentyVPos {
 		return fmt.Errorf("output range %d is not allowed", rng)
 	}
-	return nil // TODO: impl
+	Crng := C.int(rng)
+	dac.cfg.opts._chan[C.int(channel)].Range = Crng
+	dac.sendCfgToBoard(channel)
+	return nil
 }
 
-// GetRange returns the output range of the DAC in volts
-func (dac *AP236) GetRange() (OutputRange, error) {
-	return 0, nil // TODO: impl
+// GetRange returns the output range of the DAC in volts.
+// The error value is always nil
+func (dac *AP236) GetRange(channel int) (OutputRange, error) {
+	Crng := dac.cfg.opts._chan[C.int(channel)].Range
+	return OutputRange(Crng), nil
 }
 
 // SetPowerUpVoltage configures the voltage set on the DAC at power up
-func (dac *AP236) SetPowerUpVoltage(scale OutputScale) error {
+// The error is only non-nil if the scale is invalid
+func (dac *AP236) SetPowerUpVoltage(channel int, scale OutputScale) error {
 	if scale < ZeroScale || scale > FullScale {
 		return fmt.Errorf("output scale %d is not allowed", scale)
 	}
-	return nil // TODO: impl
+	dac.cfg.opts._chan[C.int(channel)].PowerUpVoltage = C.int(scale)
+	dac.sendCfgToBoard(channel)
+	return nil
 }
 
-// GetPowerUpVoltage retrieves the output range of the DAC
-func (dac *AP236) GetPowerUpVoltage() (OutputScale, error) {
-	var out OutputScale
-	return out, nil // TODO: impl
+// GetPowerUpVoltage retrieves the voltage of the DAC at power up.
+// the error is always nil
+func (dac *AP236) GetPowerUpVoltage(channel int) (OutputScale, error) {
+	Cpwr := dac.cfg.opts._chan[C.int(channel)].PowerUpVoltage
+	return OutputScale(Cpwr), nil
 }
 
 // SetClearVoltage sets the voltage applied at the output when the device is cleared
-func (dac *AP236) SetClearVoltage(scale OutputScale) error {
+// the error is only non-nil if the voltage is invalid
+func (dac *AP236) SetClearVoltage(channel int, scale OutputScale) error {
 	if scale < ZeroScale || scale > FullScale {
 		return fmt.Errorf("output scale %d is not allowed", scale)
 	}
-	return nil // TODO: impl
+	dac.cfg.opts._chan[C.int(channel)].ClearVoltage = C.int(scale)
+	dac.sendCfgToBoard(channel)
+	return nil
 }
 
 // GetClearVoltage gets the voltage applied at the output when the device is cleared
-func (dac *AP236) GetClearVoltage() (OutputScale, error) {
-	var out OutputScale
-	return out, nil // TODO: impl
+// The error is always nil
+func (dac *AP236) GetClearVoltage(channel int) (OutputScale, error) {
+	Cpwr := dac.cfg.opts._chan[C.int(channel)].ClearVoltage
+	return OutputScale(Cpwr), nil
 }
 
 // SetOverTempBehavior sets the behavior of the device when an over temp
 // is detected.  Shutdown == true -> shut down the board on overtemp
-func (dac *AP236) SetOverTempBehavior(shutdown bool) error {
-	return nil // TODO: impl
+// the error is always nil
+func (dac *AP236) SetOverTempBehavior(channel int, shutdown bool) error {
+	i := 0
+	if shutdown {
+		i = 1
+	}
+	dac.cfg.opts._chan[C.int(channel)].ThermalShutdown = C.int(i)
+	dac.sendCfgToBoard(channel)
+	return nil
 }
 
 // GetOverTempBehavior returns true if the device will shut down when over temp
-func (dac *AP236) GetOverTempBehavior() (bool, error) {
-	return false, nil // TODO: impl
+// the error is always nil
+func (dac *AP236) GetOverTempBehavior(channel int) (bool, error) {
+	Cint := dac.cfg.opts._chan[C.int(channel)].ThermalShutdown
+	return Cint == 1, nil
 }
 
 // SetOverRange configures if the DAC is allowed to exceed output limits by 5%
 // allowed == true allows the DAC to operate slightly beyond limits
-func (dac *AP236) SetOverRange(allowed bool) error {
-	return nil // TODO: impl
+// the error is always nil
+func (dac *AP236) SetOverRange(channel int, allowed bool) error {
+	i := 0
+	if allowed {
+		i = 1
+	}
+	dac.cfg.opts._chan[C.int(channel)].OverRange = C.int(i)
+	dac.sendCfgToBoard(channel)
+	return nil
 }
 
 // GetOverRange returns true if the DAC output is allowed to exceed nominal by 5%
-func (dac *AP236) GetOverRange() (bool, error) {
-	return false, nil // TODO: impl
+// the error is always nil
+func (dac *AP236) GetOverRange(channel int) (bool, error) {
+	Cint := dac.cfg.opts._chan[C.int(channel)].OverRange
+	return Cint == 1, nil
 }
 
 // SetOutputSimultaneous configures the DAC to simultaneous mode or async mode
-func (dac *AP236) SetOutputSimultaneous(simultaneous bool) error {
-	return nil // TODO: impl
+// this function will always return nil.
+func (dac *AP236) SetOutputSimultaneous(channel int, simultaneous bool) error {
+	sim := 0
+	if simultaneous {
+		sim = 1
+	}
+	// opts.chan -> opts._chan; cgo rule to replace go identifier
+	dac.cfg.opts._chan[C.int(channel)].UpdateMode = C.int(sim)
+	dac.sendCfgToBoard(channel)
+	return nil
 }
 
 // GetOutputSimultaneous returns true if the DAC is in simultaneous output mode
-func (dac *AP236) GetOutputSimultaneous() (bool, error) {
-	return false, nil // TODO: impl
+// the error value is always nil
+func (dac *AP236) GetOutputSimultaneous(channel int) (bool, error) {
+	i := int(dac.cfg.opts._chan[C.int(channel)].UpdateMode)
+	return i == 1, nil
+}
+
+// sendCfgToBoard updates the configuration on the board
+func (dac *AP236) sendCfgToBoard(channel int) {
+	C.cnfg236(&dac.cfg, C.int(channel))
+	return
 }
 
 // Output writes a voltage to a channel.
@@ -255,12 +319,37 @@ func (dac *AP236) Output(channel int, voltage float64) error {
 	return nil // TODO: impl
 }
 
+// OutputDN writes a value to the board in DN.
+// Value is of type interface{} for compatibility but must be a uint16
+// or an error will be generated
+func (dac *AP236) OutputDN(channel int, value interface{}) error {
+	v, ok := (value).(uint16)
+	if !ok {
+		return fmt.Errorf("output value is not a uint16")
+	}
+	C.wro236(&dac.cfg, C.int(channel), C.word(v))
+	return nil
+}
+
 // Flush writes any pending output values to the device
-func (dac *AP236) Flush() error {
-	return nil // TODO: impl
+func (dac *AP236) Flush() {
+	C.simtrig236(&dac.cfg)
 }
 
 // Clear soft resets the DAC, clearing the output but not configuration
-func (dac *AP236) Clear() error {
-	return nil // TODO: impl
+// the error is always nil
+func (dac *AP236) Clear(channel int) error {
+	dac.cfg.opts._chan[C.int(channel)].DataReset = C.int(1)
+	dac.sendCfgToBoard(channel)
+	dac.cfg.opts._chan[C.int(channel)].DataReset = C.int(0)
+	return nil
+}
+
+// Reset completely clears both data and configuration for a channel
+// the error is always nil
+func (dac *AP236) Reset(channel int) error {
+	dac.cfg.opts._chan[C.int(channel)].FullReset = C.int(1)
+	dac.sendCfgToBoard(channel)
+	dac.cfg.opts._chan[C.int(channel)].FullReset = C.int(0)
+	return nil
 }
