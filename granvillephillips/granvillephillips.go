1e09c91b8adb44115426e3d29690402ca07ea323
Brandon Dube <brandon.dube@jpl.nasa.gov>
2020-08-10 17:06:37 -0700

    remove junk (preserved in dead-code branch)

--- granvillephillips/granvillephillips.go
+++
@@ -1,9 +0,0 @@
-// Package granvillephillips enables working with GP375 pressure sensors.
-package granvillephillips
-
-import (
-	cp "github.jpl.nasa.gov/bdube/golab/commonpressure"
-)
-
-// NewSensor re-exports commonpressure newsensor
-var NewSensor = cp.NewSensor
--- lesker/lesker.go
+++
@@ -1,9 +0,0 @@
-// Package lesker enables working with KJC300 pressure sensors.
-package lesker
-
-import (
-	cp "github.jpl.nasa.gov/bdube/golab/commonpressure"
-)
-
-// NewSensor re-exports NewSensor from commonpressure, the verbiage is the same
-var NewSensor = cp.NewSensor
