b6e1dc87583ceb8f57264c2944ac7d73c0b274b3
Brandon Dube <brandon.dube@jpl.nasa.gov>
2022-06-06 15:55:45 -0700

    pi/mock: improve fidelity of Raw() mock

--- pi/mock.go
+++ pi/mock.go
@@ -2,9 +2,12 @@ package pi
 
 import (
 	"errors"
+	"fmt"
 	"math/rand"
+	"strconv"
 	"sync"
 	"time"
+	"unicode"
 
 	"github.jpl.nasa.gov/bdube/golab/comm"
 )
@@ -184,5 +187,138 @@ func (c *MockController) MoveRel(axis string, dPos float64) error {
 }
 
 func (c *MockController) Raw(s string) (string, error) {
+	// PI GCS2 format: (TLA = Three Letter Acronym)
+	// from<sp>to<sp>TLA<sp><?><sp>arg1<sp>arg2
+
+	// err 1 = parameter syntax error
+	// err 3 = command length out of bounds
+	if len(s) < 4 {
+		// minimum length is TLA?
+		return "", GCS2Err(3)
+	}
+
+	// pi is ascii, safe to assume uniform indexing of string (bytes)
+	// and runes
+	u := []rune(s)
+	// if FROM is specified, it does not matter to our mock
+	if unicode.IsDigit(u[0]) {
+		s = s[1:]
+		u = u[1:]
+	}
+	if s[0:1] == "" {
+		// next character had to be a space
+		s = s[1:]
+		u = u[1:]
+	} else {
+		return "", GCS2Err(1)
+	}
+	// if TO is specified, it does not matter to our mock
+	if unicode.IsDigit(u[0]) {
+		s = s[1:]
+		u = u[1:]
+	}
+	if s[0:1] == "" {
+		// next character had to be a space
+		s = s[1:]
+		u = u[1:]
+	} else {
+		return "", GCS2Err(1)
+	}
+	// now we may have skipped four characters (from, sp, to, sp); is there anything
+	// left?
+	if len(s) < 4 {
+		// minimum length is TLA?
+		return "", GCS2Err(3)
+	}
+	// no way to avoid nested switches.  Pull the reads up
+	if s[3] == '?' {
+		// read, only one argument (axis)
+		if len(s) != 7 {
+			// T L A <sp> ? <sp> axis == 7
+			return "", GCS2Err(1)
+		}
+		axis := string(s[6])
+		switch s[:3] {
+		case "VEL":
+			return returnFloatAsString(axis, c.GetVelocity)
+		case "POS":
+			return returnFloatAsString(axis, c.GetPos)
+		case "SVA":
+			return returnFloatAsString(axis, c.GetVelocity)
+		case "ONT":
+			return returnBoolAsString(axis, c.GetInPosition)
+		case "SVO":
+			return returnBoolAsString(axis, c.GetEnabled)
+		default:
+			return "", tlaNotUnderstoodInMock(s)
+		}
+	}
+	// all code in the above if returns, do not need an indent
+	// what remains is either 1 axis-arg pair or two
+	// TODO 2022-06-06 ; decided to only support one axis-argument pair for
+	// simplicity until someone has a demonstrable need for two
+	if len(s) < 5 {
+		// 6 = T L A <sp> axis
+		return "", GCS2Err(1)
+	}
+	axis := string(s[4])
+	// take care of argument-less commands
+	switch s[:3] {
+	// only one case today, maybe more in the future.  Keep a switch.
+	case "FRF":
+		return "", c.Home(axis)
+	}
+	// boolean argument commands
+	if len(s) < 7 {
+		// 7 = 5 + sp + 0 or 1
+		return "", GCS2Err(1)
+	}
+	argb := s[7] == '1' // 1 = true; 0 = false
+	switch s[:3] {
+	case "SVO":
+		if argb {
+			return "", c.Enable(axis)
+		} else {
+			return "", c.Disable(axis)
+		}
+	}
+	if s[:3] == "SPA" || s[:3] == "CCL" || s[:3] == "WPA" {
+		return "", nil // assume user knows how to use parameter manipulation
+	}
+	// everything else is a float command
+	argf, err := strconv.ParseFloat(s[7:], 64)
+	if err != nil {
+		return "", err
+	}
+	switch s[:3] {
+	case "MOV":
+		return "", c.MoveAbs(axis, argf)
+	case "MVR":
+		return "", c.MoveRel(axis, argf)
+		// case "SVA": -- SVA not supported in the mock right now
+		// return "", c.SetVoltage(axis, argf)
+	}
 	return "", NotImplemented
+}
+
+type tlaNotUnderstoodInMock string
+
+func (t tlaNotUnderstoodInMock) Error() string {
+	return fmt.Sprintf("command '%s' was not understood by Golab's PI/GCS2 mock", t)
+}
+
+func returnFloatAsString(s string, fun func(string) (float64, error)) (string, error) {
+	f, err := fun(s)
+	return strconv.FormatFloat(f, 'g', -1, 64), err
+}
+
+func returnBoolAsString(s string, fun func(string) (bool, error)) (string, error) {
+	b, err := fun(s)
+	// can clobber input now
+	if b {
+		s = "1"
+	} else {
+		s = "0"
+	}
+	return s, err
 }
